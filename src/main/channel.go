881c3de065541d77f85e56ba5edd78a437a45930
冉文杰 <510495266@qq.com>
2020-04-16 12:51:58 +0800

    update chan

--- src/main/channel.go
+++ src/main/channel.go
@@ -5,15 +5,27 @@ import "fmt"
 var ch chan int = make(chan int)
 
 func main()  {
+	//test111()
+	test123()
+}
 
+func test111()  {
 	go funA() //写到下面会死锁
 	ch <- 1
 	fmt.Println("hello")
 }
 
-func funA()  {
-
-	a := <- ch
-	println(a)
-
+func test123()  {
+	go funA()
+	closeCh()
 }
+
+func funA()  {
+	a := <- ch
+	fmt.Printf("exit %d\n" , a)
+}
+
+func closeCh(){
+	close(ch)
+}
+
