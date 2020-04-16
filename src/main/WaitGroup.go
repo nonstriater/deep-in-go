0483a78c6c0a8e9f0189fb5dbe6f70445e7d0068
冉文杰 <510495266@qq.com>
2020-04-16 20:01:55 +0800

    add closure

--- src/main/WaitGroup.go
+++ src/main/WaitGroup.go
@@ -10,12 +10,14 @@ func main() {
 	runtime.GOMAXPROCS(1)
 	wg := sync.WaitGroup{}
 	wg.Add(20)
-	//for j := 0; j < 10; j++ {
-	//	go func() {
-	//		fmt.Println("j: ", j)
-	//		wg.Done()
-	//	}()
-	//}
+
+	for j := 0; j < 10; j++ {
+		go func() {
+			fmt.Println("j: ", j)
+			wg.Done()
+		}()
+	}
+
 	for i := 0; i < 10; i++ {
 		go func(i int) {
 			fmt.Println("i: ", i)
--- src/main/gomaxprocs.go
+++ src/main/gomaxprocs.go
@@ -7,14 +7,16 @@ import (
 
 func main(){
 	runtime.GOMAXPROCS(1)
-	go func(){
-		fmt.Println("hello world")
-		// panic("hello world")  // 强制观察输出
-	}()
+
 	go func(){
 		for {
 			//fmt.Println("aaa")  // 非内联函数，这行注释打开，将导致 hello world 的输出
 		}
+	}()
+
+	go func(){
+		fmt.Println("hello world")
+		// panic("hello world")  // 强制观察输出
 	}()
 
 	select {}
