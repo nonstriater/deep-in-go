db52cd8cf9a842ba0133ce58d0a5a6cbe7a185d4
冉文杰 <510495266@qq.com>
2020-07-18 20:31:59 +0800

    gorilla mux 学习

---
+++ src/main/mux/go.mod
@@ -0,0 +1,8 @@
+module mux
+
+go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4
+	golang.org/x/tools v0.0.0-20200717024301-6ddee64345a6
+)
---
+++ src/main/mux/gorilla.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	run()
+}
+
+func run(){
+	var dir string
+	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+	flag.Parse()
+
+	// 初始化Router
+	r := mux.NewRouter()
+
+	// 静态文件路由
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
+
+	// 普通路由
+	r.HandleFunc("/", HomeHandler)
+	// 指定host
+	r.HandleFunc("/host", HostHandler).Host("localhost")
+	// 带变量的url路由
+	r.HandleFunc("/users/{id}", GetUserHandler).Methods("Get").Name("user")
+
+	url, _ := r.Get("user").URL("id", "5")
+	fmt.Println("user url: ", url.String())
+
+	// 遍历已注册的路由
+	r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		pathTemplate, err := route.GetPathTemplate()
+		if err == nil {
+			fmt.Println("ROUTE:", pathTemplate)
+		}
+		pathRegexp, err := route.GetPathRegexp()
+		if err == nil {
+			fmt.Println("Path regexp:", pathRegexp)
+		}
+		queriesTemplates, err := route.GetQueriesTemplates()
+		if err == nil {
+			fmt.Println("Queries templates:", strings.Join(queriesTemplates, ","))
+		}
+		queriesRegexps, err := route.GetQueriesRegexp()
+		if err == nil {
+			fmt.Println("Queries regexps:", strings.Join(queriesRegexps, ","))
+		}
+		methods, err := route.GetMethods()
+		if err == nil {
+			fmt.Println("Methods:", strings.Join(methods, ","))
+		}
+		fmt.Println()
+		return nil
+	})
+
+	r.Use(TestMiddleware)
+	log.Fatal(http.ListenAndServe(":3001", r))
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "this is home")
+}
+
+func HostHandler(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "the host is localhost")
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprint(w, "this is get user, and the user id is ", vars["id"])
+}
+
+
+func TestMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		fmt.Println("middleware print: ", r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+
